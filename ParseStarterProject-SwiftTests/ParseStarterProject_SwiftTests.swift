04635fd00f9b713abee200cdac3a900e609f03fe
niral28 <niralshah928@gmail.com>
2016-04-02 21:26:26 -0400

    added unit testing

---
+++ ParseStarterProject-SwiftTests/ParseStarterProject_SwiftTests.swift
@@ -0,0 +1,32 @@
+//
+//  ParseStarterProject_SwiftTests.swift
+//  ParseStarterProject-SwiftTests
+//
+//  Created by Niral Shah on 4/2/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import XCTest
+
+class ParseStarterProject_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
